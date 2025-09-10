59bd23987e6af4e24b69b40b53c379b6a3805f3c
uksoldiers123-lab <225610158+uksoldiers123-lab@users.noreply.github.com>
2025-09-09 20:42:41 -0400

    Update and rename js/auth-logic.js to public/js/auth-logic.js

--- public/js/auth-logic.js
+++ public/js/auth-logic.js
@@ -1,4 +1,3 @@
-
 <script>
   const SUPABASE_URL = 'https://gifguoyqccozlijrxgcf.supabase.co'; // replace
   const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpZmd1b3lxY2NvemxpanJ4Z2NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2MTg2MzUsImV4cCI6MjA3MjE5NDYzNX0.gwZnr8fKE7qXuLi8B5Merul3cVAXZ1r6SaWEUoAJWX0'; // replace
